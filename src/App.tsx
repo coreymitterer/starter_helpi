62eb5e4dd3b36d60936d3663aaf01d4728e8f80d
$coreymit <coreymit@udel.edu>
2024-04-11 12:51:49 -0400

    Commented out unused code to get it to merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Survey } from "./components/Survey"
 import { Pages } from "./components/Pages"
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
@@ -15,15 +14,15 @@ function App() {
   const [key, setKey] = useState<string>(keyData); //for api key input
 
   //sets the local storage item to the api key the user inputed
-  function handleSubmit() {
-    localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-  }
+  // function handleSubmit() {
+  //   localStorage.setItem(saveKeyData, JSON.stringify(key));
+  //   window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+  // }
 
-  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
-  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
-    setKey(event.target.value);
-  }
+  // //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
+  // function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
+  //   setKey(event.target.value);
+  // }
   return (
     <div className="App">
       <Pages></Pages>
