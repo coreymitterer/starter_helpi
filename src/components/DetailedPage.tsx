2aff212947f573868ec22ec2e476b674b1a7507a
Ian Duffy <ianduffy@udel.edu>
2024-04-21 21:04:51 -0400

    Made Detailed Quiz with Forms and Buttons. Updated Stylesheet

--- src/components/DetailedPage.tsx
+++ src/components/DetailedPage.tsx
@@ -1,17 +1,22 @@
 //import React, { useState } from "react";
 //import { Button, Form, OverlayTrigger, Tooltip } from "react-bootstrap";
-import "../index.scss";
+import "../index.css";
+import { DetailedQuiz } from "./DetailedQuiz";
 //import Survey from "./Survey";
 
 export function DetailedPage(): JSX.Element {
-    
+
   return (
     <div>
         <div className = "detailed">
             <h1>Detailed</h1>
             <p>Welcome to the detailed page!</p>
             <p>Take your career exploration to the next level with our Detailed Career Quiz! Dive deep into various aspects of your professional life, including your skills, values, work environment preferences, and career goals. This comprehensive quiz is tailored for you and your unique strengths and aspirations. This quiz provides a deeper dive into potential careers so you can make informed choices.</p>
+            <div className="quiz">
+              <DetailedQuiz></DetailedQuiz>
+            </div>
         </div>
     </div>
+    
     );
 }
---
+++ src/components/DetailedQuiz.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import detailedQuestionBank from './DetailedQuestionBank';
+import "../index.css";
+const QUESTIONS: string[] = detailedQuestionBank.map(question => question.question);
+const TOPICS: string[] = detailedQuestionBank.map(question => question.topic);
+const DEFAULT_QUESTION_INDEX: number = 0;
+export function DetailedQuiz(): JSX.Element {
+    const [questionIndex, setQuestionIndex] = useState<number>(DEFAULT_QUESTION_INDEX);
+    const [userResponses, setUserResponses] = useState<string[]>(new Array(QUESTIONS.length).fill(''));
+    function nextQuestion(): void {
+        setQuestionIndex(questionIndex + 1);
+    }
+    function prevQuestion(): void {
+        setQuestionIndex(questionIndex - 1);
+    }
+    function changeUserResponse(event: React.ChangeEvent<HTMLInputElement>): void {
+        const updatedResponses = [...userResponses];
+        updatedResponses[questionIndex] = event.target.value;
+        setUserResponses(updatedResponses);
+    }
+    return (
+        <div>
+            <h3>Topic: {TOPICS[questionIndex]}</h3>
+            <div>
+                <Form.Group className="mb-3" controlId="formDetailedQuestion">
+                    <Form.Label>{QUESTIONS[questionIndex]}</Form.Label>
+                    <Form.Control 
+                        type="detailedAnswer" 
+                        placeholder="Enter Response"
+                        value={userResponses[questionIndex]} 
+                        onChange={changeUserResponse} />
+                </Form.Group>
+                <Button 
+                    onClick={prevQuestion}
+                    disabled={questionIndex === 0}>
+                        Previous Question
+                </Button>
+                <Button 
+                    onClick={nextQuestion}
+                    disabled = {questionIndex === QUESTIONS.length-1}>
+                        Next Question
+                </Button>
+            </div>
+        </div>
+    );
+}
