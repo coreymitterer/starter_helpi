a3f7332431d4eaeb0026dae67775a93f69a17335
connorbutsmaller <ianduffy@udel.edu>
2024-04-24 01:29:58 -0400

    Added some animations to text on home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import "../index.css";
 import logo from "./ProfessionPilotLogo.png";
@@ -29,7 +29,31 @@ export function HomePage(): JSX.Element {
     const [isHome, setHome] = useState<boolean>(true);
     const [isBasic, setBasic] = useState<boolean>(false);
     const [isDetailed, setDetailed] = useState<boolean>(false);
+    const [index, setIndex] = useState<number>(0);
+    const careerList = [
+        "Software Developer",
+        "Doctor",
+        "Salesperson",
+        "Artist",
+        "Accountant",
+        "Truck Driver",
+        "Manager",
+        "Engineer",
+        "Teacher",
+        "Musician"
+    ];
 
+    useEffect(() => {
+        function cycleCareer(): void {
+            setIndex((index+1)%careerList.length);
+        }
+
+        const intervalId = setInterval(cycleCareer, 3000); // Calls cycleCareer every 3 seconds
+    
+        // Cleanup function to clear the interval when the component unmounts
+        return () => clearInterval(intervalId);
+    }, [index, careerList.length]);
+    
     function updateSetBasic(): void {
         setHome(false);
         setBasic(true);
@@ -44,26 +68,34 @@ export function HomePage(): JSX.Element {
 
     return (
         <div className = "home">
+            <div className = "websiteTitle">
+                <span className="title">Profession Pilot</span>
+                <div className="slogan">
+                    <div className = "sloganContainer">
+                        <p className = "sloganText">Now Boarding: <span className="listTerm">{careerList[index]}</span></p>
+                    </div>
+                </div>
+            </div>
             <div className = "main">
                 <img 
                     src = {logo}
                     alt = "Profession Pilot Logo">
                 </img>
-                <Button
-                    type="button"
-                    className="nextButton"
-                    onClick={updateSetDetailed}
-                 >
-                    <span className="button-span">Detailed</span>
-                </Button>
-                <Button
-                    type="button"
-                    className="prevButton"
-                    onClick={updateSetBasic}
-                    >
-                    <span className="button-span">Basic</span>
-                </Button>
             </div>
+            <Button
+                type="button"
+                className="nextButton"
+                onClick={updateSetDetailed}
+                >
+                <span className="button-span">Detailed</span>
+            </Button>
+            <Button
+                type="button"
+                className="prevButton"
+                onClick={updateSetBasic}
+                >
+                <span className="button-span">Basic</span>
+            </Button>
             {/* Assuring a smooth landing into your new career */}
             <div className = "features"> </div>
             <div className = "services"> </div>
