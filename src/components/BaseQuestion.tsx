57cfc29b10667c52272ec25c207132fdd27d38ec
JUNPUYIN WEI NAME <naiqi666@gmail.com>
2024-04-25 18:57:07 -0400

    Basic-Question

---
+++ src/components/BaseQuestion.tsx
@@ -0,0 +1,79 @@
+const BaseQuestion = [
+  {
+    id: 1,
+    question:
+      "Q1:I like working in a city and am willing to live in a highly populated area for work",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 2,
+    question: "Q2:I enjoy working in small groups on large projects",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 3,
+    question:
+      "Q3:I enjoy working in small groups on large projects I would rather work at a desk for the majority of my work",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 4,
+    question: "Q4:I enjoy working with my hands to get tasks done",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 5,
+    question:
+      "Q5:Technology scares me and I have no interest in working with it",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 6,
+    question: "Q6:I enjoy working with my hands to get tasks done",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 7,
+    question: "Q7:I enjoy talking with customers/clients for my work",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+  {
+    id: 8,
+    question: "Q8:I like helping people solve problems and teaching them",
+    a: "A.Strongly Agree 😃",
+    b: "B.Agree 😀",
+    c: "C.Neutral 😐",
+    d: "D.Disagree 😒",
+    e: "E.Strongly Disagree 😔",
+  },
+];
+
+export default BaseQuestion;
--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -4,135 +4,135 @@ import "../index.css";
 import Survey from "./Survey";
 import { DetailedPage } from "./DetailedPage";
 import { HomePage } from "./HomePage";
+import { BaseQuestionPage } from "./BaseQuestionPage";
 export function Pages(): JSX.Element {
-    const [isHome, setHome] = useState<boolean>(true);
-    const [isBasic, setBasic] = useState<boolean>(false);
-    const [isDetailed, setDetailed] = useState<boolean>(false);
+  const [isHome, setHome] = useState<boolean>(true);
+  const [isBasic, setBasic] = useState<boolean>(false);
+  const [isDetailed, setDetailed] = useState<boolean>(false);
 
-    let keyData = "";
-    const saveKeyData = "MYKEY";
-    const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
-    if (prevKey !== null) {
-        keyData = JSON.parse(prevKey);
-    }
+  let keyData = "";
+  const saveKeyData = "MYKEY";
+  const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+  if (prevKey !== null) {
+    keyData = JSON.parse(prevKey);
+  }
 
-    const basicTooltip = (
-        <Tooltip id="tooltip">
-          Shorter Quiz For Your Dream Career
-        </Tooltip>
-    );
+  const basicTooltip = (
+    <Tooltip id="tooltip">Shorter Quiz For Your Dream Career</Tooltip>
+  );
 
-    const detailedTooltip = (
-        <Tooltip id="tooltip">
-          Longer Quiz To Find Your Perfect Fit 
-        </Tooltip>
-    );
+  const detailedTooltip = (
+    <Tooltip id="tooltip">Longer Quiz To Find Your Perfect Fit</Tooltip>
+  );
 
-    const homeTooltip = (
-        <Tooltip id="tooltip">
-          Home Is Where The Career Quizzes Are
-        </Tooltip>
-    );
+  const homeTooltip = (
+    <Tooltip id="tooltip">Home Is Where The Career Quizzes Are</Tooltip>
+  );
 
-    function updateSetHome(): void {
-        setHome(true);
-        setBasic(false);
-        setDetailed(false);
-    }
+  function updateSetHome(): void {
+    setHome(true);
+    setBasic(false);
+    setDetailed(false);
+  }
 
-    function updateSetBasic(): void {
-        setHome(false);
-        setBasic(true);
-        setDetailed(false);
-    }
+  function updateSetBasic(): void {
+    setHome(false);
+    setBasic(true);
+    setDetailed(false);
+  }
 
-    function updateSetDetailed(): void {
-        setHome(false);
-        setBasic(false);
-        setDetailed(true);
-    }
+  function updateSetDetailed(): void {
+    setHome(false);
+    setBasic(false);
+    setDetailed(true);
+  }
 
+  const [key, setKey] = useState<string>(keyData); //for api key input
 
-    const [key, setKey] = useState<string>(keyData); //for api key input
+  //sets the local storage item to the api key the user inputed
+  function handleSubmit() {
+    localStorage.setItem(saveKeyData, JSON.stringify(key));
+    console.log(saveKeyData);
+    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+  }
 
-    //sets the local storage item to the api key the user inputed
-    function handleSubmit() {
-      localStorage.setItem(saveKeyData, JSON.stringify(key));
-      console.log(saveKeyData)
-      window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-    }
-  
-    //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
-    function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
-      setKey(event.target.value);
-    }
-    return (
+  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
+  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
+    setKey(event.target.value);
+  }
+  return (
+    <div>
+      <div className="nav">
         <div>
-            <div className="nav">
-                <div>
-                    <OverlayTrigger
-                        trigger="hover"
-                        placement="bottom"
-                        overlay={basicTooltip}
-                    >
-                        <Button
-                            type="button"
-                            className="button"
-                            onClick={updateSetBasic}
-                            disabled={isBasic}
-                        >
-                            <span className="button-span">Basic</span>
-                        </Button>
-                    </OverlayTrigger>
-                </div>
-                <div>
-                    <OverlayTrigger
-                        placement="bottom"
-                        delay={{ show: 250, hide: 400 }}
-                        overlay={homeTooltip}
-                    >
-                        <Button
-                            className="button"
-                            onClick={updateSetHome}
-                            disabled={isHome}
-                        >
-                            <span className="button-span">Home</span>
-                        </Button>
-                    </OverlayTrigger>
-                </div>
-                <div>
-                    <OverlayTrigger
-                        placement="bottom"
-                        delay={{ show: 100, hide: 100 }}
-                        overlay={detailedTooltip}
-                    >
-                        <Button className="button" onClick={updateSetDetailed} disabled={isDetailed}>Detailed</Button>
-                    </OverlayTrigger>
-                </div>
-                <div>
-                    <Survey></Survey>
-                </div>
-            </div>
+          <OverlayTrigger
+            trigger="hover"
+            placement="bottom"
+            overlay={basicTooltip}
+          >
+            <Button
+              type="button"
+              className="button"
+              onClick={updateSetBasic}
+              disabled={isBasic}
+            >
+              <span className="button-span">Basic</span>
+            </Button>
+          </OverlayTrigger>
+        </div>
+        <div>
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={homeTooltip}
+          >
+            <Button
+              className="button"
+              onClick={updateSetHome}
+              disabled={isHome}
+            >
+              <span className="button-span">Home</span>
+            </Button>
+          </OverlayTrigger>
+        </div>
+        <div>
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 100, hide: 100 }}
+            overlay={detailedTooltip}
+          >
+            <Button
+              className="button"
+              onClick={updateSetDetailed}
+              disabled={isDetailed}
+            >
+              Detailed
+            </Button>
+          </OverlayTrigger>
+        </div>
+        <div>
+          <Survey></Survey>
+        </div>
+      </div>
 
-            {isHome && (<HomePage></HomePage>)}
-            {isBasic && (
-                <div className="basic">
-                    <h1>Basic</h1>
-                    <p>Welcome to the basic page!</p>
-                    <p>Discover your career preferences and strengths with our Basic Career Quiz! Answer simple questions about your interests, skills, and goals to gain insights into potential career paths that align with your personality and aspirations. This quiz provides a starting point for your career journey.</p>
-                </div>
-            )}
-            {isDetailed && (
-              <DetailedPage></DetailedPage>
-            )}
-            <center>
+      {isHome && <HomePage></HomePage>}
+      {isBasic && <BaseQuestionPage></BaseQuestionPage>}
+      {isDetailed && <DetailedPage></DetailedPage>}
+      <center>
         <Form>
-            <Form.Label>API Key:</Form.Label>
-            <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey} size="sm" style={{ width: "200px" }} />
-            <br></br>
-            <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Insert API Key Here"
+            onChange={changeKey}
+            size="sm"
+            style={{ width: "200px" }}
+          />
+          <br></br>
+          <Button className="Submit-Button" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Form>
       </center>
-          </div>
-    );
+    </div>
+  );
 }
