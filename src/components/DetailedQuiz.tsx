b2399081afd2b4d5a8c466219992c9d8da9d3491
connorbutsmaller <ianduffy@udel.edu>
2024-05-15 19:02:27 -0400

    Fixed Quizzes

--- src/components/DetailedQuiz.tsx
+++ src/components/DetailedQuiz.tsx
@@ -48,11 +48,11 @@ export function DetailedQuiz({setReports, education, income}: DetailedString): J
         const completion = await openai.chat.completions.create({
             model: "gpt-4",
             messages: [
-                { role: "system", content: "You will be provided with a list of questions and answers, your job is to turn these questions and answers into 5 different career options with the job title and a short description of each career. You will format these 5 careers into a Json file and only a Json format" },
-                { role: "user", content: "Here is a list of questions: " + QUESTIONS.join(", ") + " And here is the combined output of answers: " + userResponses.join(", ") + "Here are the survey results that the user also inputted, Their preferred level of education is" + education + ": and here is their preferred income: " + String(income)}
+                { role: "system", content: "You will be provided with a list of questions and answers, your job is to turn these questions and answers into 5 different career options with the job title and a short description of each career. You will format these 5 careers into a Json file and only a Json format where the Job title should be under the name {title} and the description should be called {description}" },
+                { role: "user", content: "Here is a list of questions: " + QUESTIONS.join(", ") + " And here is the combined output of answers: " + userResponses.join(', ')}
             ],
         });
-        setIsLoading(false);
+        setIsLoading(false); // Set loading to false after receiving response
         setOutput(completion.choices[0]?.message.content || "");
         setReports(completion.choices[0]?.message.content || "");
         console.log(output)
